f164ff1cc10741f9774da4114435154d014a4c77
Amit Rao <amitrao99@yahoo.com>
2021-02-27 03:46:59 +0530

    added logger in middlewares in store called the configure store method and got the store in index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from '../reducers/index';
+import logger from 'redux-logger';
 let store;
 export function configureStore() {
-  store = createStore(reducer, applyMiddleware(thunk));
-
+  store = createStore(reducer, applyMiddleware(thunk, logger));
+  console.log(store);
   return store;
 }
